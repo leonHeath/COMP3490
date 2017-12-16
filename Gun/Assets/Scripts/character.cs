145067b8b47d516bdc591e092d76485f594f3baa
rawlrus <rawlrus@gmail.com>
2017-12-15 20:48:31 -0600

    health bar complete

--- Gun/Assets/Scripts/character.cs
+++ Gun/Assets/Scripts/character.cs
@@ -1,28 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI; //new
 using UnityEngine;
 
 public class character : MonoBehaviour {
 
-	public int health;
+	public float health;
+	private float max;
+	//public Text healthBar;
+	//public string heart;
+	public Image healthBar;
+
+	void Start()
+	{
+		//heart = "";
+		max = health;
+		SetHealthBar ();
+
+	}
 
 	// Update is called once per frame
 	void Update () 
 	{
-
 		if (health <= 0) 
 		{
-			//gameOver
+			//source.PlayOneShot (dieSound);
+			//Destroy (gameObject);
 		}
-		
 	}
+
+
 
 	void OnTriggerEnter(Collider other) 
 	{
 		if (other.gameObject.CompareTag ("Enemy")) 
 		{
 			health -= 1;
-
+			//heart = "";
+			SetHealthBar ();
 		}
 	}
+
+	void SetHealthBar ()
+	{
+		//for(int i = 0; i <= health-1; i++)
+		//{
+		//	heart += "<3";
+		//}
+		//healthBar.text = "Health: "+ heart;
+		healthBar.fillAmount = health/max;
+	}
+
+
+
 }

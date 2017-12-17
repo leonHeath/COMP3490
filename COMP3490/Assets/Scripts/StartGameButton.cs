062a1bd1dd92f3efbefd3c791024b15c4b2c4cd6
Shai <fridbois@myumanitoba.ca>
2017-12-17 00:55:58 -0600

    GUI is done, created script to pass player name and start game

---
+++ COMP3490/Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGameButton : MonoBehaviour {
+	public InputField inputName;
+
+	void start(){
+		inputName = gameObject.GetComponent<InputField> ();
+	}
+
+	public void assignUserName(){
+		Debug.Log (inputName.text);
+		UserData.setUserName (inputName.text);
+
+		Debug.Log (UserData.getUserName ());
+
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
